08eb8fc50e8ff12b87994cf10a103ef72782ab78
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-09-25 19:35:51 -0400

    Added Blank JPanel Template Main Class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,25 @@
 package main;
 
-public class Main {
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main extends JPanel {
+	
+	public void paintComponent (Graphics g) {
+		// Call parent constructor
+		super.paintComponent(g);
+	}
 
 	public static void main(String[] args) {
 		// This is the access point to create and draw a new tree object
-
+		JFrame frame = new JFrame("Tree Tester");
+		frame.getContentPane().add(new Main());
+		frame.setSize(1000, 1000);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
