40733d7bb4f739bf631a3808617418bc1a5d5eec
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-10-02 18:05:46 -0400

    MORE NODES!!!
     - Added the terminator node (for finishing statements and intended to end a subtree), conditional node (used for tree splits, similar to that of a conditional statement) and the Iterator Node (which acts like a loop and points to a previous node, therefore looping the tree structure)

---
+++ src/node/IteratorNode.java
@@ -0,0 +1,19 @@
+package node;
+
+import java.awt.Graphics;
+
+import node.Node;
+
+public class IteratorNode extends Node {
+
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/node/TerminatorNode.java
@@ -0,0 +1,17 @@
+package node;
+
+import java.awt.Graphics;
+
+import node.Node;
+
+public class TerminatorNode extends Node {
+
+	public void draw (Graphics g) {
+		
+	}
+
+	public String toString() {
+		return null;
+	}
+
+}
