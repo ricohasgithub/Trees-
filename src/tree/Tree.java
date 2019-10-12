9705d6dd05451c3bba2cc3162d2b25c6f3cea1c6
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-10-12 15:51:37 -0400

    Added Recursive Tree Traverse Method
     - Added a new method that allows for a recursive implementation of the Node adding algorithm (which requries tree traversing)

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -2,6 +2,7 @@ package tree;
 
 import node.*;
 import utils.*;
+import java.util.List;
 
 public class Tree {
 	
@@ -18,11 +19,60 @@ public class Tree {
 		size = 1;
 	}
 	
+	// Arguments: index is the height position within the tree (of the parent Node) and fork is the horizontal index of the index Node layer
 	public void addNode (Node next, int index, int fork) {
-		// Iterate through the entire Node-tree network until you reach a fork
-		while (start.child != null) {
+
+		// Temporary Node variable used to keep track of the current node
+		Node curr = start;
+		
+		// Tree is empty, add Node to first layer directly
+		if (curr.child.size() == 0) {
+			curr.child.add(next);
+			return;
+		}
+		
+		// Otherwise, traverse the tree and insert the Node recursively
+		traverse(curr, 0, 0, next, index, fork);
+		
+	}
+	
+	public void traverse (Node curr, int cIndex, int cFork, Node insert, int fIndex, int fFork) {
+		
+		// If the recursive algorithm has arrived at the desired insertion location within the tree structure, insert the Node and stop
+		if (cIndex == fIndex && cFork == fFork) {
+			System.out.println("Insertion Successful!");
+			curr.child.add(fFork, insert);
+			return;
+		} else if (cIndex == fIndex) {
+			// If the method has reached the layer of Nodes on the same height (index) as the destination
+			
+			// New layer, reset cFork
+			cFork = 0;
+			for (Node n : curr.child) {
+				// Traverse each child Node (increment cFork)
+				cFork++;
+				traverse(n, cIndex, cFork, insert, fIndex, fFork);
+			}
+		} else if (cIndex > fIndex) {
+			
+			// New layer, reset cFork and increment height index variable
+			cIndex++;
+			cFork = 0;
+			
+			// If the method still hasn't reached the desired layer height, keep traversing
+			for (Node n : curr.child) {
+				// Also search through all of the Nodes located under the current Node subtree and the adjacent Node's subtrees
+				for (Node nc : n.child) {
+					cFork++;
+					traverse(n, cIndex, cFork, insert, fIndex, fFork);
+				}
+			}
 			
 		}
+		
+		// All subtree Nodes reached and no viable/viable solution found, regardless, break the recursive method
+		return;
+		
 	}
 	
 	public int getSize () {
